36521e538f5b9ef024b4d0adecf9e641c0787f69
Hassan Abdelwahab <hassan-abdelwahab@hotmail.com>
2023-04-16 23:19:42 -0400

    Initialize Express server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req,res) => {
+    res.send('This is an about page')
+})
+
+app.post('/users',(req, res) => {
+    const {name} = req.body;
+    // Create user based name
+})
+
+
+app.get('/web', (req,res) => {
+
+    res.send('www.sevenseasmotors.ca')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
